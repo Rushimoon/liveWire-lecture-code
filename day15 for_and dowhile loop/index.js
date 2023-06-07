d9800e67d39a6b062551fe51cfe14a773c41e163
Rushimoon <rushikeshmoon1998@gmail.com>
2023-06-07 09:42:57 +0530

    day15

---
+++ day15 for_and dowhile loop/index.js
@@ -0,0 +1,46 @@
+// While loop 
+let i=20;
+while(i<50){
+
+   console.log(i=i+5)
+}
+
+// for loop
+
+for(let i=10 ;i>0;i=i-2){
+
+    console.log(i)
+}
+
+
+// do while loop 
+
+let name = "vaibhav" ;
+let surname = "narnaware" ;
+let fullname = name+"  "+surname
+console.log(fullname)
+
+
+console.log("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-")
+
+let store="";
+let k=1;
+do{
+    k=k+5
+    store+="**"+"["+k+"]"
+    // console.log(k);
+    
+}while(k<100)
+console.log("value of store is -"+store);
+
+
+let n=7 ;
+
+
+let jhola="";
+
+for(let i=1;i<=7;i++)
+{
+    jhola=jhola+"* ";
+    console.log(jhola);
+}
