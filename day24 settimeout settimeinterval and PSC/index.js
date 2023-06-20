ea5db9086152b3740e4f56f7f243016021482ad8
Rushimoon <rushikeshmoon1998@gmail.com>
2023-06-20 10:07:58 +0530

    up

---
+++ day24 settimeout settimeinterval and PSC/index.js
@@ -0,0 +1,53 @@
+////setInterval;
+////setTimeout;
+
+// let  j=0;
+// setInterval(function(){
+//     j=j+4
+//     console.log("set time interval value is -->"+j)
+// },1000);
+
+// let vaibhav=document.getElementById("demo");
+//     vaibhav.innerText="13245648" ;
+// let div=document.getElementById("div");
+
+// let h5=document.createElement("h1");
+   
+       
+//    div.append(h5);
+
+
+//    let hemant=document.getElementById("div").innerText;
+//    console.log(hemant)
+ 
+// setInterval(function(){
+//     j=j+4
+//    h5.innerText=j 
+// },1000);
+
+//    setTimeout(function(){
+//     j=j+4
+//     h5.innerText=j 
+// },5000);
+
+
+// setInterval(function(){
+//     j=j+4
+//    h5.innerText=j 
+// },1000);
+
+// create a time interval which should print your name and should repeat it self for every 2sec ;
+// create a time out which should  print "my first timer after 3 sec"
+
+
+setInterval(function(){
+   
+    console.log("Pratiksha")
+},2000);
+// =========================================
+
+setTimeout(function(){
+    console.log("my first timer")
+
+},3000);
+
