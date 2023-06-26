29a6c93878a004022042d2c1df0272db2789e13c
Rushimoon <rushikeshmoon1998@gmail.com>
2023-06-26 10:00:26 +0530

    up

---
+++ day27 making objects from form/index.js
@@ -0,0 +1,14 @@
+
+
+document.getElementById("form").addEventListener("submit", function(event){
+    event.preventDefault()
+   
+    let name= document.getElementById("fname").value
+    console.log(name)
+    let mobioleno=document.getElementById("mobile").value
+    console.log(mobioleno   )
+    let password=document.getElementById("pin").value
+    console.log(password)
+  });
+
+  name,surname, email , password , mobilno, add, dob
