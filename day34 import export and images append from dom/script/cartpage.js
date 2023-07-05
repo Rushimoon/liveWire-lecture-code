240823040df2568221dcad7ee7f71b44f47be56f
Rushimoon <rushikeshmoon1998@gmail.com>
2023-07-05 09:54:50 +0530

    import export

---
+++ day34 import export and images append from dom/script/cartpage.js
@@ -0,0 +1,4 @@
+import navbar from "../compopnents/navbar.js";
+
+let nav_div=document.getElementById("navbar_div");
+nav_div.innerHTML=navbar();
---
+++ day34 import export and images append from dom/script/homepage.js
@@ -0,0 +1,8 @@
+import qwe from '../compopnents/navbar.js'
+
+
+let nav_div=document.getElementById("navbar_div");
+nav_div.innerHTML=qwe();
+
+// console.log(navbar())
+
