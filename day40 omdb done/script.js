f1b0287db03e2955da41860dcc1d5156350fcbe3
Rushimoon <rushikeshmoon1998@gmail.com>
2023-07-19 10:14:35 +0530

    day50

--- day40 omdb done/script.js
+++ day40 omdb done/script.js
@@ -1,14 +1,3 @@
-let moviedata=[{"Title":"Captain Marvel","Year":"2019","imdbID":"tt4154664","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMTE0YWFmOTMtYTU2ZS00ZTIxLWE3OTEtYTNiYzBkZjViZThiXkEyXkFqcGdeQXVyODMzMzQ4OTI@._V1_SX300.jpg"},{"Title":"Ms. Marvel","Year":"2022","imdbID":"tt10857164","Type":"series","Poster":"https://m.media-amazon.com/images/M/MV5BZmQ3OTZkNDUtNTU0Mi00ZjE4LTgyNTUtY2E4NWRmNDUxMzkyXkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_SX300.jpg"},{"Title":"Marvel One-Shot: Agent Carter","Year":"2013","imdbID":"tt3067038","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BZDIwZTM4M2QtMWFhYy00N2VmLWFlMjItMzI3NjBjYTc0OTMxXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"},{"Title":"Marvel One-Shot: All Hail the King","Year":"2014","imdbID":"tt3438640","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BZGFkMTZkMDQtNzM4Yy00YWEwLTkzOWEtZTMyNDRlNmJhYWJhXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"},{"Title":"Marvel One-Shot: Item 47","Year":"2012","imdbID":"tt2247732","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BMjNlMzAxNmQtOGEwZi00NTEyLWI0NWYtMTlhNmE2YTA3ZDVhXkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"},{"Title":"Marvel One-Shot: A Funny Thing Happened on the Way to Thor's Hammer","Year":"2011","imdbID":"tt2011109","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BYmVlYTg3N2QtMWM2OS00YWQyLWI2M2MtMDc0ZjBkZjk1MTY3XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"},{"Title":"Marvel One-Shot: The Consultant","Year":"2011","imdbID":"tt2011118","Type":"movie","Poster":"https://m.media-amazon.com/images/M/MV5BNGE4YjU5MDAtYzYzMC00M2RlLTk0NDgtNDU1MjgyMGI0MjI3XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"},{"Title":"Marvel Studios: Legends","Year":"2021–","imdbID":"tt13650480","Type":"series","Poster":"https://m.media-amazon.com/images/M/MV5BMzdiNGVlZGYtMGY1Ni00OGU1LTlmYzEtZDBjYjk3OGM3YTNkXkEyXkFqcGdeQXVyNzk3NDUzNTc@._V1_SX300.jpg"},{"Title":"Marvel Studios: Assembled","Year":"2021–","imdbID":"tt14094206","Type":"series","Poster":"https://m.media-amazon.com/images/M/MV5BNWMyNWYyMmYtZjNiZi00MzFmLTg2MjYtYWEzZWY1MzBhY2I2XkEyXkFqcGdeQXVyNTE1NjY5Mg@@._V1_SX300.jpg"},{"Title":"Lego Marvel Super Heroes","Year":"2013","imdbID":"tt2620204","Type":"game","Poster":"https://m.media-amazon.com/images/M/MV5BOTA5ODA2NTI2M15BMl5BanBnXkFtZTgwNTcxMzU1MDE@._V1_SX300.jpg"}]
- localStorage.setItem("movielist",JSON.stringify(moviedata));
-
- 
-
-
-
-
- let mydata=JSON.parse(localStorage.getItem("movielist"));
-// console.log(mydata)
-// console.log(mydata[1].Title,mydata[1].Poster,mydata[0].Year);
 let container=document.getElementById("container");
 async function submited(){
    container.innerHTML="";
@@ -16,19 +5,16 @@ async function submited(){
    try{
     
     let mydata= await fetch(`https://www.omdbapi.com/?s=${inputval}&apikey=82dd9e8`)
-    let finaldata= await mydata.json();    
+
+   //  console.log(mydata)
+    let finaldata= await mydata.json(); 
+   //  console.log(finaldata.S)   
     finaldata.Search.forEach(display)
   
      }
      catch{
         console.log("eror")
      }
-
- 
-   
-
-   
-    // mydata.forEach(display);
 
 }
 
@@ -53,6 +39,20 @@ card.append(poster,title,year)
 container.append(card)
 
 }
+let id;
+function debounce(func,delay){
 
+  ifgit d){
+   clearTimeout()
+  }
+
+ id=setTimeout(function(){
+      
+     func()
+
+
+ },delay) 
+
+}
 
 
