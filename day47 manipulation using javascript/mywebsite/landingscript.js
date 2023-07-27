7636cb1e4a672e14fd257e6966b38bd8fe4a8cbc
Rushimoon <rushikeshmoon1998@gmail.com>
2023-07-27 10:50:40 +0530

    day47

---
+++ day47 manipulation using javascript/mywebsite/landingscript.js
@@ -0,0 +1,75 @@
+
+let containerdiv=document.getElementById("containerdiv");
+window.onload = submited();
+
+async function submited(){
+   containerdiv.innerHTML="";
+   
+   try{
+    
+    let mydata= await fetch("http://localhost:3000/products")
+    console.log(mydata)
+
+   //  console.log(mydata)
+    let finaldata= await mydata.json(); 
+    console.log(finaldata)   
+    finaldata.forEach(display)
+  
+     }
+     catch{
+        console.log("eror")
+     }
+
+}
+
+
+
+
+function display(el,i,arr){
+//    console.log("============================================================") 
+  console.log("elements-"+" "+el.Title +el.Year+"   index"+i)
+let card=document.createElement("div");
+card.setAttribute("class","card");
+let productImgcard=document.createElement("img");
+productImgcard.src=el.productImg
+productImgcard.setAttribute("id","productImg")
+let productName=document.createElement("h1");
+ productName.innerText=el.productName;
+ productName.setAttribute("id","productName")
+let Category=document.createElement("h2");
+Category.innerText=el.Category;
+Category.setAttribute("id","Category")
+let productPrice=document.createElement("h2");
+productPrice.innerText=el.productPrice;
+productPrice.setAttribute("id","productPrice")
+
+card.append(productImgcard,productName, productPrice,Category)
+containerdiv.append(card)
+
+}
+
+
+
+async function showWomensData(){
+
+    containerdiv.innerHTML="";
+   
+    try{
+     
+     let mydata= await fetch("http://localhost:3000/products?Category=womens")
+     console.log(mydata)
+ 
+    //  console.log(mydata)
+     let finaldata= await mydata.json(); 
+     console.log(finaldata)   
+     finaldata.forEach(display)
+   
+      }
+      catch{
+         console.log("eror")
+      }
+
+
+
+
+}
