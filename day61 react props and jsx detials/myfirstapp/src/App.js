4284d6baba81c167a694d49932ee4dab64f4dfc6
Rushimoon <rushikeshmoon1998@gmail.com>
2023-08-19 10:38:06 +0530

    props

---
+++ day61 react props and jsx detials/myfirstapp/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Navbar title="rushikesh"  data="this was the text entered"/> 
+  );
+}
+
+export default App;
