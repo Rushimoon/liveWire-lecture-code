93a8deaaf4b71737c2cde16b921ec0382244d542
Rushimoon <rushikeshmoon1998@gmail.com>
2023-08-28 10:55:57 +0530

    update

--- day65_statemanagement/my-app/src/App.js
+++ day65_statemanagement/my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './Component/Counter';
 import TodoList from './Component/TodoList';
+import Form from './Component/Form';
 
 function App() {
   return (
     <div >
        {/* <Counter/> */}
        <TodoList/>
+       <Form/>
     </div>
   );
 }
---
+++ day65_statemanagement/my-app/src/Component/Form.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+function Form(){
+    const [inputs, setInputs] = useState({});
+    const [array,setArray] =useState([]);
+    console.log(array)
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    // console.log(name+" "+value)
+    setInputs(values => ({...values, [name]: value}))
+    // console.log(inputs)
+
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs)
+    setArray(values => [...array,inputs] ) ;
+    
+
+    // alert(inputs);
+  }
+  
+    return(
+        <form onSubmit={handleSubmit}>
+        <label>Enter your name:
+        <input 
+          type="text" 
+          name="username" 
+          value={inputs.username || ""} 
+          onChange={handleChange}
+        />
+        </label>
+        <label>Enter your age:
+          <input 
+            type="number" 
+            name="age" 
+            value={inputs.age || ""} 
+            onChange={handleChange}
+          />
+          </label>
+          <label>Enter your mobile Number:
+          <input 
+            type="number" 
+            name="mobileNumber" 
+            value={inputs.mobileNumber || ""} 
+            onChange={handleChange}
+          />
+          </label>
+          <label>Enter your address:
+          <input 
+            type="text" 
+            name="address" 
+            value={inputs.address || ""} 
+            onChange={handleChange}
+          />
+          </label>
+          
+          <input type="submit" />
+      </form>
+    )
+}
+export default Form ;
