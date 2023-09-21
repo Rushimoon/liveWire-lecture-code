b73f9c2b506cc0e99481c8f123334b903932218b
Rushimoon <rushikeshmoon1998@gmail.com>
2023-09-21 17:22:15 +0530

    update

---
+++ Nodejs/day6 nodemon/index.mjs
@@ -0,0 +1,6 @@
+import chalk from "chalk";
+
+console.log("inside js file")
+console.log("inside  anothew  js file qwewqeqwe")
+console.log(chalk.red("ainoiand"))
+console.log(chalk.blue("this is blue"))
